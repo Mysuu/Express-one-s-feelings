2e65ad3c020b7a2e2fc941dd2104cb6d68456557
choilv <choi.lo.van@bunbusoft.com>
2022-08-11 16:30:31 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+  titleWeb: "Love confess",
+  introTitle: "❤ Give you ❤",
+  introDesc: `La bàn chỉ hướng Bắc.
+  Xong lại chỉ hướng Nam.
+  Tớ chỉ hướng về cậu.
+  Thứ khác tớ không ham.`,
+  btnIntro: "^^Bấm vào đây đii^^",
+  title: "Làm người yêu anh nha 🥰",
+  desc: "",
+  btnYes: "Em đồng ý 👀",
+  btnNo: "Không nha 😒😒",
+  question: "Sao lại đồng ý thế =)))",
+  btnReply: "Gửi nè 😁",
+  reply: "Sao mà từ chối được 🤣",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Tối nay 7h, anh qua đón đi chơi nha.",
+  btnAccept: "Okiiiii lun ❤",
+  messLink: "https://www.facebook.com/messages/t/100009507562481",
+};
